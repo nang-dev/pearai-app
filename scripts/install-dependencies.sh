b6c45db657d5da6ad50de77ca1852a93c0a3efbd
Nang <nathanang2000@gmail.com>
2024-05-29 12:02:20 -0400

    Added attempt to fixx packaging

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # Install dependencies for the submodule
-./extensions/pearai-submodule/scripts/install-dependencies.sh
+cd extensions/pearai-submodule
+./scripts/install-dependencies.sh
 if [ $? -ne 0 ]; then
   echo "Failed to install dependencies for the submodule"
   echo "Dependencies installation did not succeed."
   exit 1
 fi
+cd ../../
 
 # Install dependencies using yarn
 yarn install
