a23fe98238fb049255e9a963201dbd0eb9697a48
Nang <nathanang2000@gmail.com>
2024-05-29 11:38:28 -0400

    Added script progress

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-./extensions/pearai-submodule/scripts/install-dependencies.sh
+# Install dependencies for the submodule
+./install-dependencies.sh
+if [ $? -ne 0 ]; then
+  echo "Failed to install dependencies"
+  echo "Packaging did not succeed."
+  exit 1
+fi
 
+# Run yarn gulp with input arguments
 yarn gulp {input} "$1"
+if [ $? -ne 0 ]; then
+  echo "Failed to run yarn gulp"
+  echo "Packaging did not succeed."
+  exit 1
+fi
+
+# Success message
+echo "Packaging Completed Successfully! ⭐"
